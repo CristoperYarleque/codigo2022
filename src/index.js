60684c367b37c79d5ad6567833503317281b702d
Cristoper Yarleque <caypsaturno@gmail.com>
2022-03-08 21:45:46 -0500

    chore: update cors

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { archivoRouter } from "./routes/archivos.routes.js";
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-const listaBlanca = ["http://localhost:3000"];
+const listaBlanca = ["http://localhost:3000", "https://codigo2021.vercel.app"];
 
 app.use(json());
 app.use(cors({ origin: listaBlanca }));
