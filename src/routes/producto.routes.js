489eafc7ea01a9e9905fbd16dc3e9d89e8c136d3
Cristoper Yarleque <caypsaturno@gmail.com>
2022-03-08 21:28:03 -0500

    chore: testeando

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -7,16 +7,16 @@ import {
   obtenerProducto,
   obtenerProductos,
 } from "../controller/producto.controller.js";
-import { validarUsuario } from "../utils/validador.js";
+// import { validarUsuario } from "../utils/validador.js";
 
 export const productoRouter = Router();
 
-productoRouter.post("/producto", validarUsuario, crearProducto);
+productoRouter.post("/producto", crearProducto);
 productoRouter.get("/busqueda", obtenerBusqueda);
 productoRouter.get("/productos", obtenerProductos);
 
 productoRouter
   .route("/producto/:id")
   .get(obtenerProducto)
-  .put(validarUsuario, actualizarProducto)
-  .delete(validarUsuario, eliminarProducto);
+  .put(actualizarProducto)
+  .delete(eliminarProducto);
